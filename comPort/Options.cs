1ad30352c17218e2247c4dc8c44214e21fa60d50
Tadeusz Ciszewski <office@tc-soft.pl>
2021-04-30 16:25:36 +0200

    feat: Add Options.cs

---
+++ comPort/Options.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace comPort
+{
+    class Options
+    {
+		[Option("baud", Default = 9600, HelpText = "Baud rate of COM port.")]
+		public int Baud { get; set; }
+
+		[Option("data", Default = 8, HelpText = "Data bits of COM port (5-9).")]
+		public int Data { get; set; }
+
+		[Option("parity", Default = false, HelpText = "Parity bit of COM port.")]
+		public bool Parity { get; set; }
+
+		[Option("stop", Default = 1, HelpText = "Stop bits of COM port.")]
+		public int Stop { get; set; }
+
+		[Option("command", Required = true, HelpText = "Command to send.")]
+		public string Command { get; set; }
+
+
+
+		[Option("file", Required = true, HelpText = "The path of a file to data output.")]
+		public string OutputFile { get; set; }
+
+
+	}
+}
--- comPort/Program.cs
+++ comPort/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace comPort
 {
@@ -6,7 +10,29 @@ namespace comPort
     {
         static void Main(string[] args)
         {
+            var parser = new CommandLine.Parser(with => with.HelpWriter = null);
+            var parserResult = parser.ParseArguments<Options>(args);
+            parserResult
+              .WithParsed<Options>(options => Run(options))
+              .WithNotParsed(errs => DisplayHelp(parserResult, errs));
+
+        }
+
+        static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                //h.Heading = "Myapp 2.0.0-beta"; //change header
+                h.Copyright = "Copyright (c) 2019 TC-Soft.PL"; //change copyright text
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+        }
+        private static void Run(Options options)
+        {
             Console.WriteLine("Hello World!");
+            Thread.Sleep(3000);
         }
     }
 }
